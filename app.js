d046deda7701e8c28fc3309c9d1808c9ef59ae0d
Catalina-S <catalina.sanchez-m@mail.escuelaing.edu.co>
2025-04-29 21:23:21 -0500

    appjscambio

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById('MedicamentosForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
-    // Obtener los valores del formulario
+  // Obtener los valores del formulario
   const identifierSystem = document.getElementById('identifierSystem').value;
   const identifierValue = document.getElementById('identifierValue').value;
   const medicationCode = document.getElementById('medicationCode').value;
@@ -10,44 +10,44 @@ document.getElementById('MedicamentosForm').addEventListener('submit', function(
   const frequency = document.getElementById('frequency').value;
   const duration = document.getElementById('duration').value;
 
-    // Crear el objeto MedicationRequest
+  // Crear el objeto MedicationRequest
   const medicationRequest = {
-      resourceType: "MedicationRequest",
-      status: "active",
-      intent: "order",
-      medicationCodeableConcept: {
-          coding: [{
-              system: "http://www.nlm.nih.gov/research/umls/rxnorm",
-              code: medicationCode,
-              display: medicationDisplay
-          }]
-      },
-      subject: {
-          identifier: {
-              system: identifierSystem,
-              value: identifierValue
-          }
-      },
-      dosageInstruction: [{
-          text: `Tomar ${dosage} cada ${frequency} horas durante ${duration} días`
+    resourceType: "MedicationRequest",
+    status: "active",
+    intent: "order",
+    medicationCodeableConcept: {
+      coding: [{
+        system: "http://www.nlm.nih.gov/research/umls/rxnorm",
+        code: medicationCode,
+        display: medicationDisplay
       }]
+    },
+    subject: {
+      identifier: {
+        system: identifierSystem,
+        value: identifierValue
+      }
+    },
+    dosageInstruction: [{
+      text: `Tomar ${dosage} cada ${frequency} horas durante ${duration} días`
+    }]
   };
 
-    // Enviar la solicitud al backend
-    fetch('https://solicitudback.onrender.com', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(medicationRequest)
+  // Enviar la solicitud al backend
+  fetch('https://meds-backend-fjhd.onrender.com/medication-request', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(medicationRequest)
   })
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
       console.log('Success:', data);
       alert('Receta creada exitosamente');
-  })
-  .catch(error => {
+    })
+    .catch(error => {
       console.error('Error:', error);
       alert('Error al crear la receta');
-  });
+    });
 });
